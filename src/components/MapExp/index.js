a36a443711000c43032760656305f455e66aaac9
Tânia Rocha <taniapassosrocha@gmail.com>
2020-06-09 09:12:11 +0100

    Setup next.js

--- src/components/MapExp/index.js
+++ src/components/MapExp/index.js
@@ -1,34 +1,41 @@
-import React from "react";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useEffect, useState } from "react";
 
-import styles from "./index.module.css";
-
-const MapExp = () => {
+function MapExp() {
   const coords = {
     lat: 41.55,
     lng: -8.424,
     zoom: 13,
   };
+  const [leafletData, setLeafletData] = useState(null);
 
-  if (typeof window !== "undefined") {
-    return (
-      <Map
-        center={[coords.lat, coords.lng]}
-        zoom={coords.zoom}
-        className={styles.map}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[coords.lat, coords.lng]}>
-          <Popup>A custom popup.</Popup>
-        </Marker>
-      </Map>
-    );
+  useEffect(() => {
+    import("react-leaflet").then((data) => {
+      setLeafletData(data);
+    });
+  }, []);
+
+  if (!leafletData) {
+    return null;
   }
 
-  return null;
-};
+  const { Marker, MapContainer, Popup, TileLayer } = leafletData;
+
+  return (
+    <MapContainer
+      style={{ height: "400px", width: "100%" }}
+      center={[coords.lat, coords.lng]}
+      zoom={coords.zoom}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[coords.lat, coords.lng]}>
+        <Popup>A pretty CSS3 popup.</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
 
 export default MapExp;

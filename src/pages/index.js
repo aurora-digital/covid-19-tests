e80c5ed607d9e09de5205898e6115d7400f9977a
Tânia Rocha <taniapassosrocha@gmail.com>
2020-06-16 13:14:07 +0100

    Add laboratories location and popup with info

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,33 @@
-import React from "react";
-import MapExp from "../components/MapExp";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Faqs from "../components/Faqs";
+import MapExp from "../components/MapExp";
 
 import styles from "./index.module.css";
 
 export default function Home() {
+  const [labs, setLabs] = useState([]);
+
+  async function fetchLabs() {
+    const result = await axios.get(
+      "https://auroradigital-covid-labs.herokuapp.com/api/labs",
+    );
+
+    setLabs(result.data);
+  }
+
+  useEffect(() => {
+    fetchLabs();
+  }, []);
+
   return (
     <div className={styles.root}>
       <Header />
       <div className={styles.container}>
         <section id="map" className={styles.map}>
-          <MapExp />
+          <MapExp locations={labs} />
         </section>
         <section id="faqs">
           <Faqs />

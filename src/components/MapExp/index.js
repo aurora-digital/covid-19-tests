b02f3d5332be9e247567a245b84e995c27e70ab7
Tânia Rocha <taniapassosrocha@gmail.com>
2020-06-22 10:23:59 +0100

    wip

--- src/components/MapExp/index.js
+++ src/components/MapExp/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from "react";
 import MapGL, { Marker, GeolocateControl } from "react-map-gl";
+import Geocoder from "react-mapbox-gl-geocoder";
 
 const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
   c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
@@ -24,36 +25,56 @@ const MapExp = () => {
     margin: 10,
   };
 
+  const mapAccess = {
+    mapboxApiAccessToken: process.env.MAPBOX_TOKEN,
+  };
+
+  const queryParams = {
+    country: "pt",
+  };
+
+  if (map) {
+    console.log(map.current);
+  }
+
   return (
-    <MapGL
-      {...viewport}
-      width="100vw"
-      height="100vh"
-      mapStyle="mapbox://styles/mapbox/streets-v11"
-      onViewportChange={(view) => setViewport(view)}
-      mapboxApiAccessToken={process.env.MAPBOX_TOKEN}
-      ref={map}
-    >
-      <Marker latitude={41.5472535} longitude={-8.4816328}>
-        <svg
-          height={SIZE}
-          viewBox="0 0 24 24"
-          style={{
-            cursor: "pointer",
-            fill: "#d00",
-            stroke: "none",
-            transform: `translate(${-SIZE / 2}px,${-SIZE}px)`,
-          }}
-        >
-          <path d={ICON} />
-        </svg>
-      </Marker>
-      <GeolocateControl
-        style={geolocateStyle}
-        positionOptions={{ enableHighAccuracy: true }}
-        trackUserLocation
+    <div>
+      <Geocoder
+        {...mapAccess}
+        viewport={viewport}
+        hideOnSelect
+        queryParams={queryParams}
       />
-    </MapGL>
+      <MapGL
+        {...viewport}
+        width="100vw"
+        height="100vh"
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        onViewportChange={(view) => setViewport(view)}
+        mapboxApiAccessToken={process.env.MAPBOX_TOKEN}
+        ref={map}
+      >
+        <Marker latitude={41.5472535} longitude={-8.4816328}>
+          <svg
+            height={SIZE}
+            viewBox="0 0 24 24"
+            style={{
+              cursor: "pointer",
+              fill: "#d00",
+              stroke: "none",
+              transform: `translate(${-SIZE / 2}px,${-SIZE}px)`,
+            }}
+          >
+            <path d={ICON} />
+          </svg>
+        </Marker>
+        <GeolocateControl
+          style={geolocateStyle}
+          positionOptions={{ enableHighAccuracy: true }}
+          trackUserLocation
+        />
+      </MapGL>
+    </div>
   );
 };
 
